66dbf8371c6904bdd79738900d797f3e433b44db
Ricky Santiago <145139705+fizzle1620@users.noreply.github.com>
2025-04-30 22:12:22 +0800

    first

--- weather app/weather/src/weatherFolder/Weather.jsx
+++ weather app/weather/src/weatherFolder/Weather.jsx
@@ -25,7 +25,7 @@ const Weather=()=>{
             <div className="col">
                 <img src={humidity_icon} alt="" />
                 <div>
-                    <p>90 %</p>
+                    <p>89 %</p>
                     <span>Humidity</span>
                 </div>
             </div>
